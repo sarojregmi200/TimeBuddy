3f3674701d269ad0043fe73aa8f6624b402f7440
saroj regmi <sarojregmi.official@gmail.com>
2023-05-26 10:39:27 +0545

    NEW: App template

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
