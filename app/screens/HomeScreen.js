6ba7c9b776ee12876d764c9bd65e7240f791b93f
saroj regmi <sarojregmi.official@gmail.com>
2023-05-28 08:30:29 +0545

    UPDATE: HomeScreen basic setup

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -4,11 +4,13 @@ import React, { useContext } from "react";
 // importing the context data layer
 import { contextProvider } from "../context/Context";
 
+// stylesheet
+import styles from "../styles/HomeScreen.js";
+
 const HomeScreen = () => {
   const data = useContext(contextProvider);
-  console.log(data);
 
-  return <View></View>;
+  return <View style={styles.container}></View>;
 };
 
 export default HomeScreen;
