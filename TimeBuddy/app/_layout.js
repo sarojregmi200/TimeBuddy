addf9fc9feb80cb96fe4619197da2fea8824ab91
saroj regmi <sarojregmi.official@gmail.com>
2023-06-04 11:29:40 +0545

    UPDATE: index route added

---
+++ TimeBuddy/app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+// for pointing homescreen as the main index(entry) point
+// export const unstable_settings = {
+//   initialRouteName: "",
+
+// };
+
+export default layout = () => {
+  return <Stack />;
+};
