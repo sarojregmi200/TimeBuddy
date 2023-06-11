f27d459d052ddd35f57dbfd405c08f75d1ec02bc
saroj regmi <sarojregmi.official@gmail.com>
2023-06-11 20:30:35 +0545

    UPDATE: Fixed the delete btn

--- TimeBuddy/components/routine/DeleteBtn.jsx
+++ TimeBuddy/components/routine/DeleteBtn.jsx
@@ -1,40 +1,28 @@
-import { View, Text, Dimensions } from "react-native";
+import { Dimensions, Image } from "react-native";
 import React, { useContext } from "react";
 
-// styles
-import styles from "../../components/routine/style.deleteBtn.js";
-
-// ui elements
-import { Canvas, Circle, Shadow } from "@shopify/react-native-skia";
 import { datalayer } from "../../configurations/Context.js";
 
-//icon
-import Delete from "../../assets/svgs/Delete.svg";
-
 const DeleteBtn = () => {
-  const r = 30;
-  const canvasSize = 150;
+  const circleSize = 150;
   const {
     listItem: [isHold],
   } = useContext(datalayer);
   return (
-    <Canvas
-      style={{
-        width: canvasSize,
-        height: canvasSize,
-        top: Dimensions.get("screen").height - 270,
-        left: Dimensions.get("screen").width / 2 - canvasSize / 2,
-        position: "absolute",
-        zIndex: 9,
-      }}
-    >
+    <>
       {isHold.state && (
-        <Circle r={r} cx={canvasSize / 2} cy={canvasSize / 2} color="white">
-          {/* <Delete /> */}
-        </Circle>
+        <Image
+          source={require("../../assets/deletebtn.png")}
+          style={{
+            width: circleSize,
+            top: Dimensions.get("screen").height - 270,
+            left: Dimensions.get("screen").width / 2 - circleSize / 2,
+            position: "absolute",
+            zIndex: 9,
+          }}
+        />
       )}
-      <Shadow color={"rgba(245, 138, 47, 0.44)"} dx={0} dy={0} blur={20} />
-    </Canvas>
+    </>
   );
 };
 
