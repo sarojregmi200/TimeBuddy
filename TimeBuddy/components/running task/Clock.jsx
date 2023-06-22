b540ef117ad901e8ccf82b62437aeb08c24d299c
saroj regmi <sarojregmi.official@gmail.com>
2023-06-22 16:02:08 +0545

    Update: added dash and indicator to the clock

--- TimeBuddy/components/running task/Clock.jsx
+++ TimeBuddy/components/running task/Clock.jsx
@@ -1,58 +1,65 @@
-import { View, Text, Dimensions } from "react-native";
-import React from "react";
+import { View, Text, Dimensions, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 
 // styles
 import styles from "../../styles/components/tasks/style.clock.js";
 
+// moment
+import moment from "moment";
+
 const Clock = () => {
-  // individual dash dimension
-  const strokeDimensions = {
-    height: 5,
-    width: 4,
-  };
-  const deviceWidth = Dimensions.get("screen").width;
+  const numberOfDash = 50;
 
-  // indicates the number of dashes in the circle
-  const numberOfStroke = deviceWidth / 5;
+  const deviceWidth = Dimensions.get("window").width;
 
-  const minDeg = 60;
+  const center = deviceWidth / 2;
+  const radius = (deviceWidth - 50) / 2;
+  const angleIncrement = (2 * Math.PI) / numberOfDash;
+  const dashes = [];
 
-  const getUniqueDashStyle = (number) => {
-    const center = { h: (deviceWidth - 50) / 2, k: (deviceWidth - 50) / 2 };
-    const radius = (deviceWidth - 50) / 2; // -80 due to 40px padding in each sides
-    let thetaDegree = 0; // 180 since it is a semicircle
+  for (let i = 1; i < numberOfDash; i++) {
+    const angle = i * angleIncrement;
+    //   console.log({ i, angle });
+    const x = center + radius * Math.cos(angle);
+    const y = center + radius * Math.sin(angle);
 
-    const cord = {
-      x: center.h + radius * Math.cos((2 * Math.PI * number) / numberOfStroke),
-      y: center.k + radius * Math.sin((2 * Math.PI * number) / numberOfStroke),
-    };
-    if (cord.y == minDeg) {
-      // items remaining to be printed
-      const remainingCount = numberOfStroke - number;
-
-      thetaDegree = (290 / remainingCount) * number;
+    // Check if the dot is within the cut arc
+    if (i <= 5 || i >= 45) {
+      continue; // Skip drawing the dot within the cut arc
     }
-    return {
-      bottom: cord.y || 0,
-      left: cord.x,
-      // transform: [{ rotate:  }],
-      transform: [{ rotate: thetaDegree + "deg" }],
-      display: cord.y < minDeg ? "none" : "flex",
-    };
-  };
+    dashes.push({
+      x,
+      y,
+      background: "rgba(92, 188, 168, 1)",
+    });
+  }
   return (
     <View style={styles.strokeContainer}>
-      {Array(numberOfStroke)
-        .fill("x")
-        .map((_, count) => {
-          return (
-            <View
-              style={[styles.circleBorderDash, getUniqueDashStyle(count)]}
-              key={count}
-            ></View>
-          );
-        })}
-      {console.log("****************")}
+      {dashes.map((dash, count) => {
+        return (
+          <View
+            style={[
+              styles.circleBorderDash,
+              {
+                top: dash?.y,
+                left: dash?.x,
+                backgroundColor: dash?.background,
+              },
+            ]}
+            key={count}
+          ></View>
+        );
+      })}
+
+      <Image
+        source={require("../../assets/currentTimeStroke.png")}
+        style={{
+          position: "absolute",
+          top: dashes[20].y - 30,
+          left: dashes[20].x - 30,
+        }}
+      />
+      {console.log("********************************************")}
     </View>
   );
 };
--- TimeBuddy/styles/components/tasks/style.clock.js
+++ TimeBuddy/styles/components/tasks/style.clock.js
@@ -5,18 +5,19 @@ const clockWidth = Dimensions.get("screen").width;
 export default StyleSheet.create({
   circleBorderDash: {
     backgroundColor: "rgba(164, 83, 67, 0.33)",
-    height: 6,
-    width: 4,
+    height: 4,
+    width: 6,
     borderRadius: 3,
     position: "absolute",
-    marginLeft: 25,
   },
 
   strokeContainer: {
     width: clockWidth,
     height: clockWidth,
     position: "relative",
-    // justifyContent: "center",
-    // alignItems: "center",
+    transform: [{ rotate: "90deg" }],
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: clockWidth / 2,
   },
 });
