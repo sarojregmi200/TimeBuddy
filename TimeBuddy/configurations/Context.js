8e4c33f061b4eac54d43f67ba8964d37bfb91c60
saroj regmi <sarojregmi.official@gmail.com>
2023-06-23 11:37:04 +0545

    Update: task filter on the basis of end time and tgl btn status complete

--- TimeBuddy/configurations/Context.js
+++ TimeBuddy/configurations/Context.js
@@ -27,7 +27,12 @@ const Context = ({ children }) => {
   // user
   const [user, setUser] = useState({ userId: null });
 
+  // state that contains the currently running tasks
   const [task, setTask] = useState();
+
+  // seconds in one hour, and 1 min
+  const SecondsInHour = 3600;
+  const secondInMin = 60;
 
   // after data being updated to db
   const fetchDbData = () => {
@@ -64,19 +69,45 @@ const Context = ({ children }) => {
   // a function that accepts all the routines and then extracts the current task from the routine
   const extractCurrentTask = (routines) => {
     // all the tasks in the object form
-    const tasks = [];
+    let tasks = [];
     // looping through all the routines
     routines.forEach((routine, index) => {
       // converting all the tasks in array and then pushing to the tasks array.
       tasks.push(...(JSON.parse(routine?.tasks) || []));
     });
 
-    // seconds in one hour, and 1 min
-    const SecondsInHour = 3600;
-    const secondInMin = 60;
+    // filter outs the task that are off and keeping that are on
+    tasks = tasks.filter((task) => {
+      return task.isOn === true;
+    });
+
+    // applying filter based on time and removing the task that have been complete
+    tasks.forEach((task, index) => {
+      // calculates and returns the starting and ending time of the task provided in seconds
+      const taskTime = calculateTime(task); // object that contains starting and ending time of the task
+
+      // current time in seconds
+      const currentTimeInSeconds =
+        new Date().getHours() * SecondsInHour +
+        new Date().getMinutes() * secondInMin;
+
+      // removing all the task which have already ended
+
+      // if current time is smaller it means the time has already past the end of the task and the task had to be completed in the past
+      if (currentTimeInSeconds < taskTime.end) {
+        tasks.pop(task); // remove the task from the array
+        return;
+      }
+    });
+
+    // applying bubble sort algorithm to find the task which is running or will run shortly.
+  };
+
+  const calculateTime = (task) => {
+    // calculating the starting and the ending time of the provided task
 
     // starting time of the task
-    const rawStartingTime = tasks[0]?.time?.first;
+    const rawStartingTime = task.time?.first;
     const startingTimeInSeconds =
       parseInt(rawStartingTime.split(":")[0]) * SecondsInHour +
       parseInt(
@@ -84,9 +115,19 @@ const Context = ({ children }) => {
       ) *
         secondInMin;
 
-    const currentTimeInSeconds =
-      new Date().getHours() * SecondsInHour +
-      new Date().getMinutes() * secondInMin;
+    // ending time of the task
+    const rawEndingTime = task.time?.second;
+    const endingTimeInSeconds =
+      parseInt(rawEndingTime.split(":")[0]) * SecondsInHour +
+      parseInt(
+        rawEndingTime.replace("PM", "").replace("AM", "").split(":")[1].trim()
+      ) *
+        secondInMin;
+
+    return {
+      start: startingTimeInSeconds,
+      end: endingTimeInSeconds,
+    };
   };
 
   return (
